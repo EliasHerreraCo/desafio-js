3dc889b4720748658050bc7cfb88dc1bd8a29546
Elias Herrera <elias.herrera.co@gmail.com>
2021-10-09 00:10:50 -0500

    Se añadio modal

---
+++ desafio-js/main.js
@@ -0,0 +1,9 @@
+// Crear post
+
+// 1. Seleccionar el boton
+// 2. Crear el evento y comprobar que realice accion
+// 3. Despligue de modal al darle click al boton
+
+$("#create-modal").click(()=> {
+    console.log("Click desde Jquery")
+})
